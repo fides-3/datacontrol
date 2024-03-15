695e10b53031b51e208b692d1a2d9cfa88760926
ntoiti <fidesluckia36@gmail.com>
2024-03-15 08:19:36 +0300

    first commit

---
+++ dartcontrolflowhwrk.dart
@@ -0,0 +1,14 @@
+
+import 'dart:io';
+void main(){
+  print ("enter number");
+  int num=int.parse(stdin.readLineSync()!);
+  if (num>10){
+    print (" $num is greater than 10");
+  }
+  else if(num<10){
+    print("$num is less than 10");  }
+  else{
+    print(" $num is equal to 10");
+  }
+}
